09e3187f0bfe941ef426c9cb9fd3b2570edefe02
Zied-Snoussi <ziedsnoussi.tn@gmail.com>
2021-09-12 13:52:24 +0100

    Ajoutez des fichiers projet.

---
+++ Escape Sequence.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Welcome to C++\n";// \n traj3ek lel star
+	std::cout << "Welcome to \tC++";// \t ta3ml 8 espace wra b3athhom
+}
---
+++ First project in c++.cpp
@@ -0,0 +1,9 @@
+/*
+#include <iostream>
+int main()
+{
+	std::cout << "Welcome to C++";
+	std::cout << "Zied";
+	return 0;//ba3d return 0 may5th chy toul yo5rej
+	std::cout << "I am here";
+}*/
